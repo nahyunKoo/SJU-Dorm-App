075b87d09b3ea33490bbe68f251526ac3fa229a8
nahyunKoo <angelcindy09@gmail.com>
2024-07-09 21:07:52 +0900

    init

---
+++ SJUDorm-backend/src/main/java/com/example/sjudormbackend/SjuDormBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.sjudormbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SjuDormBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SjuDormBackendApplication.class, args);
+    }
+
+}
---
+++ SJUDorm-backend/src/test/java/com/example/sjudormbackend/SjuDormBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sjudormbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SjuDormBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
