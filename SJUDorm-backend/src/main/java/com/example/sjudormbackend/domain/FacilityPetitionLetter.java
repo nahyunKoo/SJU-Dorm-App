7bdcc9e423892a645cfc260e668963857b2def66
nahyunKoo <angelcindy09@gmail.com>
2024-07-27 11:38:35 +0900

    refactor : Column 추가

--- SJUDorm-backend/src/main/java/com/example/sjudormbackend/domain/FacilityPetitionLetter.java
+++ SJUDorm-backend/src/main/java/com/example/sjudormbackend/domain/FacilityPetitionLetter.java
@@ -5,21 +5,28 @@ import lombok.Data;
 
 import java.util.Date;
 
-@Entity
-@Data
+@Entity @Data
+@Table(name = "facility_petition_letter")
 public class FacilityPetitionLetter {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "facility_petition_letter_id")
     private int facilityPetitionLetterId;
+
     @Column(name = "status")
     private boolean status;
+
     @Column(name = "room_number")
     private int roomNumber;
+
     @Column(name = "title")
     private String title;
+
     @Column(name = "name")
     private String name;
+
     @Column(name = "created_at")
     private Date createdAt;
+
     @Column(name = "content")
     private String content;
 }
