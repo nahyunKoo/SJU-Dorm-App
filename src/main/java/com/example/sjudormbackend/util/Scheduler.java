2e2eb79a41c4ff681d6dd7ec3b70f4f5d24db708
nahyunKoo <angelcindy09@gmail.com>
2024-08-07 19:01:34 +0900

    refactor : scheduler 에서 기존 db 삭제 추가

--- src/main/java/com/example/sjudormbackend/util/Scheduler.java
+++ src/main/java/com/example/sjudormbackend/util/Scheduler.java
@@ -1,5 +1,6 @@
 package com.example.sjudormbackend.util;
 
+import com.example.sjudormbackend.repository.MenuRepository;
 import com.example.sjudormbackend.service.MenuService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,11 +13,15 @@ public class Scheduler {
 
     @Autowired
     private MenuService menuService;
+    @Autowired
+    private MenuRepository menuRepository;
 
+    //식단 크롤링 매주 오전 1시 월요일마다
     @Scheduled(cron = "0 0 1 * * MON")
     public void schedulerCrawlMenu(){
         try {
-            menuService.crawlMenu();
+            menuRepository.deleteAll(); //기존 db 내용 모두 삭제
+            menuService.crawlMenu();    //크롤링
             log.info("Menu data has been successfully saved.");
         } catch (Exception e) {
             e.printStackTrace();
